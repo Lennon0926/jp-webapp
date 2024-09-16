ba28e346e0caa86fcd5af4f699cbc9a31debc663
EdzelOrtiz <edzel.ortiz@upr.edu>
2024-09-16 09:34:33 -0400

    ip-510 front end done

--- web_app/urls.py
+++ web_app/urls.py
@@ -84,6 +84,7 @@ urlpatterns = [
     re_path(r"^IP-480-transportacion-qtr/$", views.IP_480_qtr, name="IP-480-qtr"),
     re_path(r"^IP-480a-transportacion-aerea-qtr/$", views.IP_480a_qtr, name="IP-480a-qtr"),
     re_path(r"^IP-490-correos-y-mensajeros-qtr/$", views.IP_490_qtr, name="IP-490-qtr"),
+    re_path(r"^IP-510-informatica-qtr/$", views.IP_510_qtr, name="IP-510-qtr"),
 
 
 
--- web_app/views.py
+++ web_app/views.py
@@ -79,6 +79,7 @@ from src.formularios.quaterly.ingreso_neto_qtr.form_ip_440g_qtr import IP_440g_q
 from src.formularios.quaterly.ingreso_neto_qtr.form_ip_480a_qtr import IP_480a_qtr
 from src.formularios.quaterly.ingreso_neto_qtr.form_ip_480_qtr import IP_480_qtr
 from src.formularios.quaterly.ingreso_neto_qtr.form_ip_490_qtr import IP_490_qtr
+from src.formularios.quaterly.ingreso_neto_qtr.form_ip_510_qtr import IP_510_qtr
 
 def home(request):
     return render(request, "home.html")
