e5c449e63a3b6341ca4a7b92ecfd777953e697b6
EdzelOrtiz <edzel.ortiz@upr.edu>
2024-09-25 09:20:43 -0400

    ip_540j front end done

--- web_app/views.py
+++ web_app/views.py
@@ -88,6 +88,7 @@ from src.formularios.quaterly.ingreso_neto_qtr.form_ip_530_qtr import IP_530_qtr
 from src.formularios.quaterly.ingreso_neto_qtr.form_ip_540_qtr import IP_540_qtr
 from src.formularios.quaterly.ingreso_neto_qtr.form_ip_540a_qtr import IP_540a_qtr
 from src.formularios.quaterly.ingreso_neto_qtr.form_ip_540v_qtr import IP_540v_qtr
+from src.formularios.quaterly.ingreso_neto_qtr.form_ip_540j_qtr import IP_540j_qtr
 
 def home(request):
     return render(request, "home.html")
